af0e57dfcb3eeceb123496044c065eb6f6f5b15b
Leandro Tiberio <leandrostiberio@gmail.com>
2022-11-09 22:31:47 -0300

    Operadores Aritmeticos e Logicos

---
+++ exercicio1.1.js
@@ -0,0 +1,25 @@
+var num1 = 15;
+var num2 = 5;
+var soma = num1 + num2;
+var subtracao = num1 - num2;
+var multiplicacao = num1 * num2;
+var divisao = num1 / num2;
+
+var num3 = 80;
+var num4 = 3;
+
+var divisaoComResto = num3 % num4; // resultado com resto
+
+console.log("Valor da Soma é : ", soma);
+console.log("Valor da Subtração é : ", subtracao);
+console.log("Valor da Multiplicação é : ", multiplicacao);
+console.log("Valor da Divisão é : ", divisao);
+console.log("Valor da Soma, somando mais 5 ", soma + 5);
+console.log("Valor com resto da Divisão é : ", divisaoComResto);
+console.log("Vlor da Soma, subtraindo nemos 3 :", soma -3 );
+
+
+
+
+
+
---
+++ exercicio1.2.js
@@ -0,0 +1,10 @@
+if ("se caminho 5km e corro 2km, passo o dia bem") {
+    "caminho_5km && corro_2km, passo_o_dia_bem"
+}
+if ("se levando cedo ou antes das 8h, saio tranquilo") {
+    "levando_cedo || antes_das_8h, saio_tranquilo"
+}
+if ("se termino o trabalho ou não tenho compromisso saio tarde") {
+    "! termino_o_trabalho || ! tenho_compromisso_saio_tarde"
+}
+
